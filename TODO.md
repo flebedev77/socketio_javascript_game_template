c5b761ec11a72e03c3e720d58d7f45a6a121f0a8
flebedev77 <flebedev77@outlook.com>
2024-11-03 22:52:34 +1030

    Added todo.md

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# multiplayer_snake_todo
+ - [x] Add food being dropped after exessive sprinting
+ - [ ] Add basic multiplayer
+ - [ ] Add camera scrolling
+ - [ ] Add the ability to kill other players
+ - [ ] Add a menu 
--- public/src/core/init.js
+++ public/src/core/init.js
@@ -20,24 +20,10 @@ export default function init() {
     globals.local_player = new Snake(window.innerWidth/2, window.innerHeight/2, 10, 20, 10);    
     globals.local_player.is_local_player = true;
 
-    // window.addEventListener("mousemove", function(e) {
-    //     // globals.local_player.position.x = e.x;
-    //     // globals.local_player.position.y = e.y;
-
-    //     const move_dir_vector = Vector2.normalized(new Vector2(
-    //         globals.local_player.position.x - e.x,
-    //         globals.local_player.position.y - e.y
-    //     ));
-
-    //     globals.local_player.move_direction = move_dir_vector;
-    // });
-
-    
-
     apply_input_listeners();
     
 
-    for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < 30; i++) {
         globals.food_arr.push(new Food(Math.random() * window.innerWidth, Math.random() * window.innerHeight, 10));
     }
 }
